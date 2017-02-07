3af1016354c72e398abb4db6ebdc875631d48e2a
Saqlain Gardezi <saqlaingardezi86@gmail.com>
2017-02-07 12:55:48 +0500

    Schema defined for location

---
+++ app_server/models/locations.js
@@ -0,0 +1,39 @@
+var mongoose=require('mongoose');
+
+//			openingTimesSchema
+var openingTimeSchema=new mongoose.Schema({
+	days:{type: String, required:true},
+	opening: String,
+	closing: String,
+	closed:{type:String, required:true}
+});	
+
+//			reviewSchema
+var reviewSchema=new mongoose.Schema({
+	author: String,
+	rating: {type: Number, "default": 0, min:0, max: 5, required:true},
+	reviewText: String,
+	createdOn:{type:Date, "default":Date.now},
+});
+
+ /* 		Main Schema 	*/
+var locationSchema=new mongoose.Schema({
+	name: {type: String,
+			required: true},
+	address: String,
+	rating: {type: Number,
+			"default": 0,
+			min: 0,
+			max: 5},
+	facilities: [String],
+
+/* 		Use 2dsphere to add support for GeoJSON longitude and latitude coordinate pair	*/
+	coords: {type:[Number], index:'2dsphere'}, 
+
+//			Reference subSchemas to add nested subDocuments
+	openingTimes: [openingTimeSchema],
+	reviews:[reviewSchema]
+});
+
+// 			Compile schema into model
+mongoose.model('Location', locationSchema);
